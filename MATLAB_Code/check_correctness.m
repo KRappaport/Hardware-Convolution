e3defb7874b3a44816cb8d74c11065d9e9a95c8c
KRappaport <ymr994@gmail.com>
2018-05-17 01:03:04 +0300

    Added support for performing 2D convolution on 3D inputs and kernels.

--- MATLAB_Code/check_correctness.m
+++ MATLAB_Code/check_correctness.m
@@ -1,15 +1,18 @@
 % Check results, of our convolution function against MATLAB's, for correctness
 
-in = rand(11,11);
-B = ones(11);
+in = rand(11,11,11);
+B = ones(11,11,11);
+mat_result = zeros(11,11,11);
 
 for k = 1:20
     for k_s = 1:2:33
-        ker = rand(k_s,k_s);
+        ker = rand(k_s,k_s,11);
 
         result = conv_2d_naive(in,ker);
 
-        mat_result = convn(in,ker,'same');
+        for z = 1:11
+            mat_result(:,:,z) = convn(in(:,:,z),ker(:,:,z),'same');
+        end
 
         A = ismembertol(result, mat_result, 1E-12);
         if (isequal(A,B) == 0)
