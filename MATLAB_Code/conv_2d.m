54250505353786fa1b75f079a826ba8bab7847d8
KRappaport <ymr994@gmail.com>
2018-06-25 14:57:45 +0300

    Changed the outer most loop to the inner most, that way it conforms with the multi core version and cuts down on time since it allows for the bounds to be calculated only once for all matrices along the third dimension

--- MATLAB_Code/conv_2d.m
+++ MATLAB_Code/conv_2d.m
@@ -17,53 +17,50 @@ function resultant = conv_2d(input,kernel)
     resultant = zeros(in_row_lgth, in_column_lgth);
 
     % Actual convolution starts here:
-    % First for loop sets the third dimension, location in the third dimension
-    % refers to a 2D matrix that will be convolved (the convolution is 2D the
-    % third dimension can be seen as just a list of 2D matrices and the convolution
-    % only takes place between the first two dimensions of input and kernel for
-    % equal third dimension indices).
-    % Second 2 for loops determine the element of the result to find
-    % and the final 2 match up the proper input and kernel elements.
+    % First 2 for loops determine the element of the result to find
+    % and the second 2 match up the proper input and kernel elements.
+    % The final inner most for loop applies the 2D convolution to each of the
+    % matrices along the third dimension and sums the results.
     % In total 5 nested for loops.
 
-    for result_depth = 1:in_depth
-        for result_row = 1:in_row_lgth
-            ker_base_row = ker_center_row + result_row;
+    for result_row = 1:in_row_lgth
+        ker_base_row = ker_center_row + result_row;
+
+        % Set start row value such that input and kernel are in bounds
+        if ker_base_row > ker_row_lgth
+            start_row = ker_base_row - ker_row_lgth;
+        else
+            start_row = 1;
+        end
+
+        % Set end row value such that input and kernel are in bounds
+        last_row = ker_base_row - 1;
+        if last_row > in_row_lgth
+            last_row = in_row_lgth;
+        end
+
+        for result_column = 1:in_column_lgth
+            ker_base_column = ker_center_column + result_column;
 
             % Set start row value such that input and kernel are in bounds
-            if ker_base_row > ker_row_lgth
-                start_row = ker_base_row - ker_row_lgth;
+            if ker_base_column > ker_column_lgth
+                start_column = ker_base_column - ker_column_lgth;
             else
-                start_row = 1;
+                start_column = 1;
             end
 
             % Set end row value such that input and kernel are in bounds
-            last_row = ker_base_row - 1;
-            if last_row > in_row_lgth
-                last_row = in_row_lgth;
+            last_column = ker_base_column - 1;
+            if last_column > in_column_lgth
+                last_column = in_column_lgth;
             end
 
-            for result_column = 1:in_column_lgth
-                ker_base_column = ker_center_column + result_column;
+            for in_row = start_row:last_row
+                ker_row = ker_base_row - in_row;
 
-                % Set start row value such that input and kernel are in bounds
-                if ker_base_column > ker_column_lgth
-                    start_column = ker_base_column - ker_column_lgth;
-                else
-                    start_column = 1;
-                end
+                for in_column = start_column:last_column
 
-                % Set end row value such that input and kernel are in bounds
-                last_column = ker_base_column - 1;
-                if last_column > in_column_lgth
-                    last_column = in_column_lgth;
-                end
-
-                for in_row = start_row:last_row
-                    ker_row = ker_base_row - in_row;
-
-                    for in_column = start_column:last_column
-
+                    for result_depth = 1:in_depth
                         resultant(result_row, result_column) ...
                             = resultant(result_row, result_column) ...
                             + (input(in_row, in_column, result_depth) ...
