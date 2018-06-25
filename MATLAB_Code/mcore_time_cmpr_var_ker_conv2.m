007033d3b0e71f549a630c0c01a5aad83fcfb1e2
KRappaport <ymr994@gmail.com>
2018-06-25 15:21:13 +0300

    Increased image and kernel sizes

--- MATLAB_Code/mcore_time_cmpr_var_ker_conv2.m
+++ MATLAB_Code/mcore_time_cmpr_var_ker_conv2.m
@@ -12,7 +12,7 @@ end
 
 %diff kernals
 
-in = rand(33,33,11);
+in = rand(499,499,11);
 
 if ((exist('ncores')==0) || (isnumeric(ncores)==0) || (ncores <= 0))
     ncores = 2;
@@ -21,8 +21,8 @@ else
 end
 
 
-conv_2d_time= zeros(1,17);
-conv_2d_mult_core_time = zeros(1,17);
+conv_2d_time= zeros(1,250);
+conv_2d_mult_core_time = zeros(1,250);
 
 
 statuss = 'Status:    ';
@@ -32,7 +32,7 @@ fprintf([statuss, updstat]);
 
 for run_number = 1:run_amount
     time_index_ker = 1;
-    for k_s = 1:2:33
+    for k_s = 1:2:499
         ker = rand(k_s,k_s,11);
 
         tic;
@@ -63,7 +63,7 @@ conv_2d_mult_core_time = conv_2d_mult_core_time./run_amount;
 
 % regular plot take
 
-k_s = 1:2:33;
+k_s = 1:2:499;
 
 mcore_var_ker(1) = figure;
 plot(k_s,conv_2d_time.*1E3);
