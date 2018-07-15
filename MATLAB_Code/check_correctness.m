0f9648e41f23164146c39f129c9c06b912b8e682
KRappaport <ymr994@gmail.com>
2018-07-15 13:10:29 +0300

    Added the conv2d function to check_correctness

--- MATLAB_Code/check_correctness.m
+++ MATLAB_Code/check_correctness.m
@@ -1,21 +1,24 @@
 % Check results, of our convolution function against MATLAB's, for correctness
 
 in = rand(11,11,11);
-B = ones(11,11,11);
+C = ones(11,11,11);
 mat_result = zeros(11,11,11);
 
 for k = 1:20
     for k_s = 1:2:33
         ker = rand(k_s,k_s,11);
 
-        result = conv_2d_naive(in,ker);
+        result1 = conv_2d_naive(in,ker);
+
+        result2 = conv2d(in,ker);
 
         for z = 1:11
             mat_result(:,:,z) = convn(in(:,:,z),ker(:,:,z),'same');
         end
 
-        A = ismembertol(result, mat_result, 1E-12);
-        if (isequal(A,B) == 0)
+        A = ismembertol(result1, mat_result, 1E-12);
+        B = ismembertol(result2, mat_result, 1E-12);
+        if (isequal(A,B,C) == 0)
             disp('Results do not match up!')
             return
         end
