631d6e76e0bc8b782e61b52b1548ea35f98ff3dc
KRappaport <ymr994@gmail.com>
2018-07-19 20:07:55 +0300

    Moved where it informs of success so that only occurs once at end

--- MATLAB_Code/check_correctness.m
+++ MATLAB_Code/check_correctness.m
@@ -28,9 +28,9 @@ for k = 1:20
         if (isequal(A,B,C,D,true_array) == 0)
             disp('Results do not match up!')
             return
-        else
-            disp('Results match up');
         end
 
     end
 end
+
+disp('Results match up');
