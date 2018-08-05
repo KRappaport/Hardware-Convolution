a2a6a1e24fc7d92eef9007ef2e1187f4fa203d63
KRappaport <ymr994@gmail.com>
2018-08-05 10:42:08 +0300

    Able to zero 2 elements at a time in a block due to use of true 2 port BRAM

--- FPGA_Code/HLS/init_delay_line.c
+++ FPGA_Code/HLS/init_delay_line.c
@@ -2,11 +2,13 @@
 
 
 void init_delay_line(float delay_line[KERNEL_DIM-1][MAX_IMG_WIDTH-2], unsigned short delay_end){
+#pragma HLS inline
     unsigned short i,k;
 
     for (i = 0; i < KERNEL_DIM_1; i++) {
 #pragma HLS UNROLL
         for (k = 0; k <= delay_end; k++) {
+#pragma HLS UNROLL factor=2 skip_exit_check
             delay_line[i][k] = 0;
         }
     }
