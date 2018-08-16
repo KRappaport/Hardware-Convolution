6bf2369e178ade51312596a2f6a6ba5baa537564
KRappaport <ymr994@gmail.com>
2018-08-16 15:50:43 +0300

    Can now properly transfer the kernel over to the FPGA

--- FPGA_Code/SDK/set_ker.c
+++ FPGA_Code/SDK/set_ker.c
@@ -1,13 +1,17 @@
 #include "data_handling.h"
 #include "xparameters.h"
 
-void set_ker(float *kernel[], unsigned short dimension, unsigned short depth) {
-    float *kernel_base_addr = XPAR_BRAM_0_BASEADDR;
-    int i, k;
+void set_ker(float *kernel, unsigned short dimension, unsigned short depth) {
+    float *kernel_base_addr[11*MAX_KER_SIZE];
+    int i, k, j=0;
 
     for (i = 0; i < depth; i++) {
         for (k = 0; k < (dimension*dimension); k++) {
-            kernel_base_addr[(i*MAX_KER_SIZE)+k] = kernel[i][k];
+            kernel_base_addr[j++] = (XPAR_BRAM_0_BASEADDR) + (((i*MAX_KER_SIZE)+k)*4);
         }
+    }
+
+    for (i = 0; i < (depth*dimension*dimension); i++) {
+    	*kernel_base_addr[i] = kernel[i];
     }
 }
