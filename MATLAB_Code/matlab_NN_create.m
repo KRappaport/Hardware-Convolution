6095d2a1f41aeb7eb3f8d5ea0f1a69f59adabbb0
KRappaport <ymr994@gmail.com>
2018-09-02 18:42:22 +0300

    Explanation of the input arguements

--- MATLAB_Code/matlab_NN_create.m
+++ MATLAB_Code/matlab_NN_create.m
@@ -1,3 +1,10 @@
+% Use to create a neural net from MATLAB.  The aruments of the function are
+% input_W_H for the image dimensions (square image only), input_C for the depth,
+% kernel_dim for the kernel dimensions (square odd kernel only), n_kernels for
+% the number of kernels to create, padding_size in order to keep the result
+% dimensions the same as the input image, and seed in order to be able to
+% recreate the same kernel
+
 function net = matlab_NN_create(input_W_H, input_C, kernel_dim, n_kernels, padding_size, seed)
     rand('seed', seed);
     weights = rand([ kernel_dim kernel_dim input_C ]);
@@ -11,7 +18,7 @@ function net = matlab_NN_create(input_W_H, input_C, kernel_dim, n_kernels, paddi
               regressionLayer('Name','output')
               ];
     lgraph = layerGraph(layers);
-    % plot(lgraph)
+
     new_output_dim = (2*padding_size+input_W_H-kernel_dim)/stride+1;
     XTrain = rand([input_W_H input_W_H input_C 1000]);
     YTrain = rand([new_output_dim new_output_dim n_kernels 1000]);
