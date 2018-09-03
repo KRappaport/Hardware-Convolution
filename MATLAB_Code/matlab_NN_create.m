690f9546ad452593e341cfd44f3ac65dd7ddfbfd
KRappaport <ymr994@gmail.com>
2018-09-03 16:45:17 +0300

    Changed to use smaller training set and produce kernels between 1 and -1

--- MATLAB_Code/matlab_NN_create.m
+++ MATLAB_Code/matlab_NN_create.m
@@ -7,7 +7,7 @@
 
 function net = matlab_NN_create(input_W_H, input_C, kernel_dim, n_kernels, padding_size, seed)
     rand('seed', seed);
-    weights = rand([ kernel_dim kernel_dim input_C ]);
+    weights = rand([ kernel_dim kernel_dim input_C ])*2-1.0;
     stride = 1;
     convla = convolution2dLayer(kernel_dim,n_kernels,'Name', 'conv2d','Stride',[stride stride], 'Padding', [padding_size padding_size padding_size padding_size]);
     convla.Weights = weights;
@@ -20,13 +20,16 @@ function net = matlab_NN_create(input_W_H, input_C, kernel_dim, n_kernels, paddi
     lgraph = layerGraph(layers);
 
     new_output_dim = (2*padding_size+input_W_H-kernel_dim)/stride+1;
-    XTrain = rand([input_W_H input_W_H input_C 1000]);
-    YTrain = rand([new_output_dim new_output_dim n_kernels 1000]);
-
+    num_training_samples = 2;
+    XTrain = rand([input_W_H input_W_H input_C num_training_samples]);
+    YTrain = rand([new_output_dim new_output_dim n_kernels num_training_samples]);
 
     options = trainingOptions('sgdm',...
         'MaxEpochs',1,...
+        'InitialLearnRate', realmin,...
         'Shuffle','every-epoch',...
-        'Verbose',false,...
-        'Plots','training-progress');
+        'Verbose',0,...
+        'L2Regularization',0.5,...
+        'MiniBatchSize', num_training_samples);
+
     net = trainNetwork(XTrain,YTrain,lgraph,options);
