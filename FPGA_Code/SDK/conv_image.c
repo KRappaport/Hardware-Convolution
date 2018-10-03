943cc382142d3ae81e1f572921dbd46a715067c4
KRappaport <ymr994@gmail.com>
2018-10-03 09:58:46 +0300

    Added comments to the code

--- FPGA_Code/SDK/conv_image.c
+++ FPGA_Code/SDK/conv_image.c
@@ -12,13 +12,16 @@ double conv_image(XAxiDma *axidma, float *image, int image_size, float *result, 
     int status;
     XTime start, end;
 
-
+// Flush the data cache to make sure that the data in the RAM is up to date and
+// stale data isn't transferred to the FPGA.
     Xil_DCacheFlushRange((unsigned)image, image_size*sizeof(float));
     Xil_DCacheInvalidateRange((unsigned)result, result_size*sizeof(float));
 
     printf("Convolution starting...\t\t");
+    // Start the timer.
     XTime_GetTime(&start);
 
+// Initiate transfer of the image to the FPGA.
     status = XAxiDma_SimpleTransfer(axidma, (u32)image, image_size*sizeof(float), XAXIDMA_DMA_TO_DEVICE);
     if (status != XST_SUCCESS) {
         printf("ERROR: Image transfer failed!\n");
@@ -27,6 +30,7 @@ double conv_image(XAxiDma *axidma, float *image, int image_size, float *result, 
         return(-1.0f);
     }
 
+// Intitate the transfer of the convolution results into RAM
     status = XAxiDma_SimpleTransfer(axidma, (u32)result, result_size*sizeof(float), XAXIDMA_DEVICE_TO_DMA);
     if (status != XST_SUCCESS) {
         printf("ERROR: Result transfer failed!\n");
@@ -36,13 +40,18 @@ double conv_image(XAxiDma *axidma, float *image, int image_size, float *result, 
     }
 
     while (XAxiDma_Busy(axidma, XAXIDMA_DEVICE_TO_DMA)) {
-        /* code */
+        /* Loop here until all of the results have been received. */
     }
+
+    // Stop the timer
     XTime_GetTime(&end);
     printf("Convolution completed!\n");
 
+// Invalidate the data cache for the results so that next time used it will
+// ake the fresh data from the RAM.
     Xil_DCacheInvalidateRange((unsigned)result, result_size*sizeof(float));
 
+// Calculate the time for the convolution and return it.  
     double exec_time = (double)(end - start)/COUNTS_PER_SECOND;
     return(exec_time);
 }
--- FPGA_Code/SDK/main.c
+++ FPGA_Code/SDK/main.c
@@ -12,6 +12,10 @@
 #include "inits.h"
 #include "data_handling.h"
 
+// If TEST is defined then it will compare results obtained from the FPGA to
+// those found in the file specified in the variable "result_file".  If it is
+// not defined then the results will be written back to a file with the name
+// set by the variable "result_file".
 #ifndef TEST
 #define TEST
 #endif
@@ -28,10 +32,13 @@ int main() {
 
     int status;
 
+// Use these variables to specify the file names for the kernel, image, and
+// results.
     char *kernel_file;
     char *image_file;
     char *result_file;
 
+// Initialize the pointer for the convolution block.
     XConv2d instptr;
     status = xconv2d_init(&instptr);
     if (status != XST_SUCCESS) {
@@ -39,6 +46,7 @@ int main() {
         exit(-1);
     }
 
+// Initialize the pointer for the AXI DMA device.
     XAxiDma axidma;
     status = dma_init(&axidma);
     if (status != XST_SUCCESS) {
@@ -46,6 +54,7 @@ int main() {
         exit(-1);
     }
 
+// Mount the SD card as the file system.
     FATFS fs;
     status = file_system_init(&fs);
     if (status != XST_SUCCESS) {
@@ -53,6 +62,7 @@ int main() {
         exit(-1);
     }
 
+// Load the kernel from the file.
     int kernel_size;
     float *kernel;
     kernel_size = read_kernel(&kernel, kernel_file);
@@ -60,10 +70,11 @@ int main() {
         exit(-1);
     }
 
+// Transfer the kernel to the FPGA.
     set_ker(kernel, kernel_size, kernel_dim);
     free(kernel);
 
-
+// Read the image from the file.
     int image_size;
     float *image;
     image_size = read_image(&image, image_file);
@@ -71,9 +82,11 @@ int main() {
         exit(-1);
     }
 
+// Set the image dimensions for the convolution block.
     XConv2d_Set_wdth(&instptr, image_dim[0]);
     XConv2d_Set_hght(&instptr, image_dim[1]);
 
+// Check if the convolution block is ready and start it.
     printf("Starting Conv2D...\n");
     if (XConv2d_IsReady(&instptr)) {
         XConv2d_Start(&instptr);
@@ -84,6 +97,7 @@ int main() {
     }
     printf("Started.\n");
 
+// Allocate memory for the results.
     float *result;
     int result_size = image_dim[0]*image_dim[1];
     result = (float*)calloc(result_size, sizeof(float));
@@ -93,6 +107,7 @@ int main() {
         exit(-1);
     }
 
+// Perform the convolution using the FPGA.
     double conv_exec_time = conv_image(&axidma, image, image_size, result, result_size);
     if (conv_exec_time == -1.0f) {
         printf("ERROR: Transfer of either the image or result failed!\n");
@@ -104,7 +119,9 @@ int main() {
 
     free(image);
 
+// When TEST is not defined...
 #ifndef TEST
+// Results are written to a file.
     status = write_result(result, image_dim, result_size, result_file);
     if (status == -1) {
         free(result);
@@ -112,8 +129,9 @@ int main() {
     }
 #endif
 
-
+// When TEST defined...
 #ifdef TEST
+// Load in comparison results from a file
     float *cmpr_result;
     int cmpr_result_size;
     cmpr_result_size = read_result(&cmpr_result, result_file);
@@ -127,6 +145,7 @@ int main() {
         exit(-1);
     }
 
+// Compare results from the file to the results recieved from the FPGA.
     unsigned int i;
     for (i = 0; i < cmpr_result_size; i++) {
         if (((result[i] - cmpr_result[i]) > 0.005) || ((result[i] - cmpr_result[i]) < -0.005)) {
@@ -143,6 +162,10 @@ int main() {
 
 
     free(result);
+
+// Unmount the SD card.
+    f_mount(0, "0:/", 0);
+    free(&fs);
 
     return 0;
 }
